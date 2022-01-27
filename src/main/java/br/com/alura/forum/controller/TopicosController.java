f2b43f04f0c7bfd8819a312e5f58280030f98002
ViniciosG <viniciossantanag@gmail.com>
2022-01-27 09:43:16 -0300

    POST

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,36 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
+
+
 @RestController
+@RequestMapping(value = "/topicos")
 public class TopicosController {
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		if(nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
@@ -26,8 +40,15 @@ public class TopicosController {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDto.converter(topicos);
 		}
-
+	}
+	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository); 
+		topicoRepository.save(topico);
 		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico)); 
 	}
 	
 	
