512d5127b3ebab8112507ea81c1990242133586a
ViniciosG <viniciossantanag@gmail.com>
2022-01-28 09:55:46 -0300

    paginação e ordenação

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,11 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +20,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import br.com.alura.forum.controller.dto.DetalhesTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
@@ -36,16 +41,25 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
+	
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	//http://localhost:8080/topicos?page=0&size=3
+	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso,
+			@PageableDefault(sort="id", direction=Direction.ASC, page = 0, size=10) Pageable  paginacao) {
+		
+		
+//	    http://localhost:8080/topicos?pagina=0&qte=3&ordenacao=id ou qualquer atributo data,nome...etc
+		//Pageable paginacao = PageRequest.of(pagina, qte, Direction.ASC, ordenacao );
+		
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
 		} else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.converter(topicos);
 		}
 	}
+	
 	
 	@PostMapping
 	@Transactional
