6f19e785d30188e2248d0c470b3cf11cb4efe5cd
ViniciosG <viniciossantanag@gmail.com>
2022-02-01 16:34:40 -0300

    testes automatizados

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import junit.framework.Assert;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
